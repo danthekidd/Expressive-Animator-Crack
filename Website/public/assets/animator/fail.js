24df0255248e6b91f92139620a9cc721cfb5aa2f
Dan <133232755+danthekidd@users.noreply.github.com>
2024-12-13 17:54:30 -0500

    not needed

--- Website/public/assets/animator/fail.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("expr-overlay").setAttribute("error",""),document.getElementById("expr-loading-text").innerHTML='Your browser is not supported. Use a modern and up-to-date browser or <a href="https://expressive.app/account/downloads/">download the app</a>.',plausible("BrowserUnsupported",{props:{app_name:"animator",app_version:"2024.1.1",unsupported_reason:"no-module"}});
